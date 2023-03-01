aab7ec71bc50909eac890be0d02264c6d95540fc
Toby Dickinson Williams <tobyDickinson@users.noreply.github.com>
2023-03-01 12:19:24 +0000

    refactor: formatting

--- src/repository/note-repository.ts
+++ src/repository/note-repository.ts
@@ -1,4 +1,4 @@
-import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
+import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
 
 export interface NoteCreate {
   text: string;
@@ -9,17 +9,9 @@ export interface Note extends NoteCreate {
 }
 
 const client = new ApolloClient({
-  uri: 'https://actual-tapir-37.hasura.app/v1/graphql',
+  uri: "https://actual-tapir-37.hasura.app/v1/graphql",
   cache: new InMemoryCache(),
 });
-
-export const SAVE_NOTE = gql(/* GraphQL */ `
-  mutation saveNote($note: RocketInput!) {
-    saveRocket(rocket: $rocket) {
-      model
-    }
-  }
-`);
 
 export async function saveNote(note: NoteCreate): Promise<void> {
   await client.mutate({
@@ -31,7 +23,7 @@ export async function saveNote(note: NoteCreate): Promise<void> {
           }
         }
       }
-    `
+    `,
   });
 }
 
@@ -43,7 +35,8 @@ export async function getNotes(): Promise<Note[]> {
           id
           text
         }
-      }`
+      }
+    `,
   });
 
   const notes: Note[] = result.data.notes;
@@ -52,13 +45,15 @@ export async function getNotes(): Promise<Note[]> {
 
 export async function viewTypes(): Promise<void> {
   const result = await client.query({
-    query: gql`{
-      __schema {
-        types {
-          name
+    query: gql`
+      {
+        __schema {
+          types {
+            name
+          }
         }
       }
-    }`
+    `,
   });
 
   console.log(result);
