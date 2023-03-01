a5a9808ee13860b77f7e8a11f3b70c6ae2abae59
Toby Dickinson Williams <tobyDickinson@users.noreply.github.com>
2023-03-01 12:16:23 +0000

    feat: adds view notes capability

--- src/pages/ViewNotes.tsx
+++ src/pages/ViewNotes.tsx
@@ -8,33 +8,24 @@ import {
   TableRow,
 } from "@mui/material";
 import React from "react";
-import { getNotes } from "../repository/note-repository";
+import { getNotes, Note } from "../repository/note-repository";
 
-function createData(
-  name: string,
-  calories: number,
-  fat: number,
-  carbs: number,
-  protein: number
-) {
-  return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  createData("Eclair", 262, 16.0, 24, 6.0),
-  createData("Cupcake", 305, 3.7, 67, 4.3),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
-];
-
-async function fetchData() {
-  const notes = await getNotes();
-}
-
-export default class ViewNotesPage extends React.Component {
+export default class ViewNotesPage extends React.Component<
+  {},
+  { notes: Note[] }
+> {
   constructor(props: any) {
     super(props);
+
+    this.state = { notes: [] };
+
+    this.fetchData = this.fetchData.bind(this);
+    this.fetchData();
+  }
+
+  async fetchData() {
+    const notes = await getNotes();
+    this.setState({ notes: notes });
   }
 
   render() {
@@ -43,26 +34,22 @@ export default class ViewNotesPage extends React.Component {
         <Table sx={{ minWidth: 650 }} aria-label="simple table">
           <TableHead>
             <TableRow>
-              <TableCell>Dessert (100g serving)</TableCell>
-              <TableCell align="right">Calories</TableCell>
-              <TableCell align="right">Fat&nbsp;(g)</TableCell>
-              <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-              <TableCell align="right">Protein&nbsp;(g)</TableCell>
+              <TableCell align="right">ID</TableCell>
+              <TableCell>Note content</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-            {rows.map((row) => (
+            {this.state.notes.map((row) => (
               <TableRow
-                key={row.name}
+                key={row.id}
                 sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
               >
-                <TableCell component="th" scope="row">
-                  {row.name}
+                <TableCell align="right" component="th" scope="row">
+                  {row.id}
                 </TableCell>
-                <TableCell align="right">{row.calories}</TableCell>
-                <TableCell align="right">{row.fat}</TableCell>
-                <TableCell align="right">{row.carbs}</TableCell>
-                <TableCell align="right">{row.protein}</TableCell>
+                <TableCell component="th" scope="row">
+                  {row.text}
+                </TableCell>
               </TableRow>
             ))}
           </TableBody>
